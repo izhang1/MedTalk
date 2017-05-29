c21a1396679f3b1ee5d50a5bdd24905e60336206
Ivan Zhang <ivanzhangemail@gmail.com>
2017-05-29 11:20:33 -0400

    HomeActivity UI
    * Added Search functionality to toolbar
    
    * Added RecyclerView to main activity
    
    * Implemented Tab View

--- app/src/main/java/app/izhang/medtalk/Homeactivity.java
+++ app/src/main/java/app/izhang/medtalk/Homeactivity.java
@@ -2,6 +2,8 @@ package app.izhang.medtalk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Homeactivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class Homeactivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homeactivity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        return true;
     }
 }
